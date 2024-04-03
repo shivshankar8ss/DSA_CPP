6a67667f5fca62f343eb0aa03e7e30f21d05c829
shivshankar8ss <shivshankar8w@gmail.com>
2024-04-04 00:29:12 +0530

    heap

---
+++ heap/remove_stone_to_minimize_total.cpp
@@ -0,0 +1,27 @@
+// class Solution
+// {
+// public:
+//     int minStoneSum(vector<int> &piles, int k)
+//     {
+//         priority_queue<int> maxHeap;
+//         for (int i = 0; i < piles.size(); i++)
+//         {
+//             maxHeap.push(piles[i]);
+//         }
+
+//         while (k--)
+//         {
+//             int tp_ele = maxHeap.top();
+//             maxHeap.pop();
+//             tp_ele = tp_ele - floor(tp_ele / 2);
+//             maxHeap.push(tp_ele);
+//         }
+//         int sum = 0;
+//         while (!maxHeap.empty())
+//         {
+//             sum += maxHeap.top();
+//             maxHeap.pop();
+//         }
+//         return sum;
+//     }
+// };
